48246f0f06db2b7b899512dcb71eefed068affa7
Vishnu Raj <vishnumraj@gmail.com>
2020-04-08 23:25:37 +1000

    add initial app structure

---
+++ app/src/main/java/com/vishnuraj/newsfeed/NewsFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.vishnuraj.newsfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+}
